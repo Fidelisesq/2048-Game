ed53e46ac241a5ee5d50608754289b3596b06f0b
Fidelis Ikoroje <fidelis.ikoroje@gmail.com>
2025-07-19 20:33:04 +0100

    Terraform Deployment - s3 Backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+  
+  backend "s3" {
+    bucket = "foz-terraform-state-bucket"
+    key    = "2048-game/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
